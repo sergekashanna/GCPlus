ed28f3fdd2d8d9ed35c9b6ca47dcc239d8a9f82a
sergekashanna <156314663+sergekashanna@users.noreply.github.com>
2024-02-06 14:29:32 +0100

    Add files via upload

---
+++ GC_Encode_IID.m
@@ -0,0 +1,22 @@
+function [x,n,N,K,q] = GC_Encode_IID(u,l,c1,c2,t)
+    k=numel(u);
+    K=ceil(k/l);
+    N=K+c1+c2;
+    q=2^l;
+    u=[u(1:(K-1)*l),zeros(1,K*l-numel(u)),u((K-1)*l+1:end)];
+    A=reshape(u,l,K);
+    U=bi2de(fliplr(A'));
+    rsEncoder = comm.RSEncoder(N,K,'BitInput',false);
+    primPolyDegree = l;
+    rsEncoder.PrimitivePolynomialSource = 'Property';
+    rsEncoder.PrimitivePolynomial = de2bi(primpoly(primPolyDegree,'nodisplay'),'left-msb');
+    X=rsEncoder(U);
+    x=reshape(transpose(fliplr(de2bi(X,l))),1,numel(X)*l);
+    if(mod(k,l)~=0)
+        x((K-1)*l+1:K*l-mod(k,l))=[];
+    end
+    check_par=x(end-c2*l+1:end);
+    x(end-c2*l+1:end)=[];
+    x=[x,repelem(check_par,t)];
+    n=numel(x);
+end
---
+++ main_IID.m
@@ -0,0 +1,53 @@
+%Code parameters
+k=133;
+c=10; c1=8; c2=2; 
+l=7;
+len_last=mod(k-1,l)+1;
+K=ceil(k/l);
+t=3;
+
+%Decoder setting
+lim=5;
+depth=zeros(1,lim);
+depth(1)=1; depth(2)=1; depth(3)=0;
+secondary=1;
+
+
+%Simulation setup
+total_iter=1e3;
+Pe=0.001:0.001:0.01;
+Pd=Pe/3; Pi=Pe/3; Ps=Pe/3; 
+%Pi=Pe/7; Pd=2*Pe/7; Ps=4*Pe/7; 
+
+% Pre-computing some patterns
+Patterns_primary=burst_patterns(K+c1,c1); 
+Patterns_secondary=preCompute_Patterns(depth,K,len_last,lim,c1);
+disp("Done precomputing patterns");
+
+%Initalizations
+failure=zeros(1,length(Pe));
+errors=zeros(1,length(Pe));
+
+
+%Main
+for j=1:length(Pe)
+    %Initalize counters
+    countS=0; %succesful decoding
+    countF=0; %decoding failure uhat empty
+    parfor iter=1:total_iter
+        u=randi([0,1],1,k);
+        [x,n,N,K,q] = GC_Encode_IID(u,l,c1,c2,t);
+        y=binary_channel(n,x,Pd(j),Pi(j),Ps(j));
+        uhat = GC_Decode_IID(y,n,k,l,N,K,c,c1,c2,q,secondary,len_last, lim, Patterns_primary, Patterns_secondary,t);
+        if(isequal(uhat,u))
+            countS=countS+1;
+        elseif(isempty(uhat))
+            countF=countF+1;
+        end
+    end
+    failure(j)=countF/total_iter;
+    errors(j)=1-(countS/total_iter);
+    disp(['Edit Error Probability: ' num2str(Pe(j))  ', Decoding failure rate: ' num2str(failure(j)) ', Total decoding error rate: ' num2str(errors(j))]);
+end
+figure(1)
+semilogy(Pe,errors)
